eaff03c100c0df5f8051416d78ad42879a6fc275
Cauã <124628936+devcabo@users.noreply.github.com>
2025-11-20 19:35:08 -0300

    AL#04

---
+++ AL#04.c
@@ -0,0 +1,31 @@
+/*daça um programa que leia os valores para largura, comprimento e altura de um
+paralelepípedo retângulo e calcule o seu volume.
+
+Universidade dederal do Espírito Santo
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    // Variáveis de tipo real
+
+    int a, b, c;
+    int v;
+    // Armazenar valores
+    printf("Digite o valor de a: \n");
+    scanf("%d", &a);
+
+    printf("Digite o valor de b: \n");
+    scanf("%d", &b);
+
+    printf("Digite o valor de c: \n");
+    scanf("%d", &c);
+
+    // Lógica
+    v = a * b * c;
+
+    // Impressão do resultado
+    printf("O resultado do volume do paralelepipedo é: %d", v);
+}
