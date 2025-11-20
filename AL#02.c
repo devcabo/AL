3aab54e0b32e988d7806f1fb7478cc8748d30248
Cauã <124628936+devcabo@users.noreply.github.com>
2025-11-20 19:18:34 -0300

    AL#02

--- AL#02.c
+++ AL#02.c
@@ -10,17 +10,20 @@ int main(void)
     int n;
     int soma = 0;
 
+    //loop para ler os números
     for (int i = 0; i < 10; i++)
     {
         printf("Digite um número: ");
         scanf("%d", &n);
 
+        // condição ímpar
         if (n % 2 != 0)
         {
             soma += n;
         }
     }
-
+    
+    //impressão do resultado
     printf("A soma dos números ímpares é: %d\n", soma);
 
     return 0;
