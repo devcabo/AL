a41d9e4f99bbc0684b82f0717baac103ca3fe5fe
Cauã <124628936+devcabo@users.noreply.github.com>
2025-11-20 20:44:26 -0300

    AL#14

---
+++ AL#14.c
@@ -0,0 +1,25 @@
+/*Faça um algoritmo para calcular quantas ferraduras são necessárias para equipar
+todos os cavalos comprados para um haras.
+
+Universidade Federal do Espírito Santo
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    // Variáveis reais
+    int ferradura = 4, cavalo;
+
+
+    // Armazenamento de valores
+    printf("Quantos cavalos foram comprados?: \n");
+    scanf("%d", &cavalo);
+
+    // Lógica
+    ferradura *= cavalo;
+
+    // Impressão do resultado
+    printf("A quantidade de ferradura é: %d\n", ferradura);
+}
